fa3b359d1ab1b9608e5f4ab96daa28dbcdbc4380
bebo <ibrahimimran96@gmail.com>
2022-08-24 00:14:11 -0400

    inquirer enhancements

--- app.js
+++ app.js
@@ -11,11 +11,19 @@ const inquirer = require('inquirer');
 // });
 
 const promptUser = () => {
-  return inquirer.prompt([{
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
-    },
+  return inquirer.prompt([ {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name? (Required)',
+    validate: nameInput => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log('Please enter your name!');
+        return false;
+      }
+    }
+  },
     {
       type: 'input',
       name: 'github',
